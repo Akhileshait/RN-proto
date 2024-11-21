518c3c284e45b6a812b4f6fde4c505a0a23fafad
Akhileshait <akhileshpatidar989368@gmail.com>
2024-11-21 13:09:56 +0530

    first commit

---
+++ app/LoginScreen.tsx
@@ -0,0 +1,97 @@
+import React, { useState, useEffect } from "react";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation, useRouter } from "expo-router";
+import {setItem, getItem} from "./utils/AsyncStorage.js";
+
+const LoginScreen = () => {
+  const router = useRouter();
+  const [mobileNo, setMobileNo] = useState<string>("");
+  const otpServiceDown = false;
+
+  useEffect(() => {
+    const fetchMobileNumber = async () => {
+      try {
+        const mobileNum = await getItem("mobileNo"); 
+        if (mobileNum) {
+          setMobileNo(JSON.parse(mobileNum));
+        }
+      } catch (error) {
+        console.error("Error fetching mobile number:", error);
+      }
+    };
+
+    fetchMobileNumber(); 
+  }, []);
+  
+
+  const handleLogin = async () => {
+    if (mobileNo) {
+      setItem("mobileNo", mobileNo);
+      try {
+        const resp = await fetch(`http://gbmp.spring.money:3000/auth/otp?mobile_number=${mobileNo}`);
+        console.log("User Mobile no is:",mobileNo, "Response received:",resp);
+        if (resp && resp.status === 200) {
+          router.push("/otp-verify");
+        }
+      } catch (error) {
+        console.error("error", error);
+      }
+    }
+  };
+
+  const isButtonActive = mobileNo.length === 10;
+
+  return (
+    <View className="flex-1 bg-[#f8eeda] px-5 py-6 md:items-center">
+      <View className="max-w-md">
+    <Ionicons name="arrow-back" size={24} color="#333" className="mb-4" />
+
+    <Text className="text-gray-700 text-2xl font-semibold mt-10">Login with your mobile</Text>
+    <Text className="text-gray-600 text-base font-normal">
+      Enter your mobile number to get started or continue your investment journey.
+    </Text>
+
+    <View className="flex flex-col mt-8 mb-8 bg-white px-4 py-6 rounded-3xl">
+      <Text className="text-gray-800 text-sm font-medium">Mobile Number</Text>
+      <View className="flex-row mt-4 items-center border border-gray-300 rounded-lg px-3 py-2 mb-4">
+        <Ionicons name="call-outline" size={20} color="#A0A0A0" />
+        <TextInput
+          className="w-full focus:outline-none"
+          placeholder="Enter your mobile number"
+          keyboardType="phone-pad"
+          maxLength={10}
+          value={mobileNo}
+          onChangeText={setMobileNo}
+        />
+      </View>
+      <View className="flex-row items-center bg-[#EDF2FF] p-3 rounded-lg">
+        <Ionicons name="link-outline" size={20} color="#5A67D8" />
+        <Text className="text-xs text-indigo-600 ml-2 p-2">
+          Your Money Plan Report is linked to your mobile number so that you can track your goal progress anytime.
+        </Text>
+      </View>
+    </View>
+
+    <TouchableOpacity
+      className={`py-4 rounded-lg ${
+        isButtonActive ? "bg-indigo-600" : "bg-indigo-200"
+      }`}
+      onPress={handleLogin}
+      disabled={!isButtonActive}
+    >
+      <Text
+        className={`text-center text-base font-semibold ${
+          isButtonActive ? "text-white" : "text-gray-500"
+        }`}
+      >
+        Login
+      </Text>
+    </TouchableOpacity>
+    </View>
+  </View>
+  );
+};
+
+
+export default LoginScreen;
---
+++ app/otp-verify.tsx
@@ -0,0 +1,196 @@
+
+import React, { useEffect, useRef, useState } from "react";
+import { View, Text, TextInput, TouchableOpacity, TextInputKeyPressEventData } from "react-native";
+import { useNavigation, useRouter } from "expo-router";
+import Checkbox from 'expo-checkbox';
+import {setItem, getItem} from "./utils/AsyncStorage.js";
+
+export default function OtpVerify() {
+  const router = useRouter();
+  const inputRefs = useRef<(TextInput | null)[]>([]);
+  const [otp, setOtp] = useState(Array(6).fill(""));
+  const [isVerifyOtpEnabled, setIsVerifyOtpEnabled] = useState<boolean>(false);
+  const [isTncChecked, setIsTncChecked] = useState<boolean>(false);
+  const logedInSuccessfully = false;
+  const incorrectOtp = false;
+  const otpServiceDown = false;
+
+  const handleInputChange = (text:string, ind:number) =>{
+    const newOtp = [...otp];
+    newOtp[ind] = text;
+    setOtp(newOtp);
+
+    if (text && ind < otp.length - 1) {
+      inputRefs.current[ind + 1]?.focus();
+    }
+  }
+
+  const handleVerifyOtp = async () => {
+    const mobileNo = await getItem("mobileNo");
+    console.log(mobileNo, otp.join(""));
+    if (!mobileNo) {
+      router.push("/LoginScreen");
+      return;
+    }
+    if (otp.join("").length !== 6) {
+      return;
+    }
+
+    const resp = await fetch("http://gbmp.spring.money:3000/user/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        is_tnc_agreed: true,
+        mobile_number: mobileNo,
+        otp: otp.join(""),
+      }),
+    });
+
+    const respJson = await resp.json();
+
+    if (respJson && respJson.statusCode === 200) {
+      console.log("Logged in successfully")
+    } else {
+      console.error("Error logging in", respJson);
+    }
+  };
+
+  const handleKeyPress = (e:any, idx:number) => {
+    if (e.nativeEvent.key === "Backspace" && idx > 0 && !otp[idx]) {
+      inputRefs.current[idx - 1]?.focus(); // Focus previous input on backspace
+    }
+  };
+
+
+  useEffect(() => {
+    if (otp.join("").length === 6 && isTncChecked) {
+      setIsVerifyOtpEnabled(true);
+    } else {
+      setIsVerifyOtpEnabled(false);
+    }
+  }, [otp, isTncChecked]);
+
+  return (
+    <View className="flex-col  bg-[#f8eeda] bg-gradient-background pt-4 px-4 min-h-screen md:items-center">
+
+      <View className="max-w-md">
+
+      <Text className="text-gray-700 text-2xl font-semibold mt-10">
+        Login with your mobile
+      </Text>
+      <Text className="text-gray-600 text-base font-normal">
+        Enter your mobile number to get started or continue your investment
+        journey.
+      </Text>
+      <View className="flex flex-col mt-8 bg-white px-4 py-6 rounded-3xl">
+        {!logedInSuccessfully && (
+          <>
+            <Text className="text-gray-800 text-lg font-medium">
+              Enter OTP to verify
+            </Text>
+            {otpServiceDown ? (
+              <View className="p-4 rounded-2xl bg-error-25 mt-4">
+                <Text className="text-error-600 text-base font-normal">
+                  The OTP service is down, please try again in few minutes.
+                </Text>
+              </View>
+            ) : (
+              <>
+                <Text className="text-gray-600 text-sm font-normal mt-4 ">
+                  Enter the OTP sent to the registered mobile number 93XXXXX434
+                  to verify your account.
+                </Text>
+                <View className="flex-row gap-4 mt-4 justify-center">
+                  {Array(6)
+                    .fill(0)
+                    .map((_, idx) => (
+                      <TextInput
+                        key={idx}
+                        ref={(ref) => (inputRefs.current[idx] = ref)}
+                        keyboardType="numeric"
+                        maxLength={1}
+                        onKeyPress={(e) => handleKeyPress(e, idx)}
+                        className={`h-[52px] w-[44px] p-3 text-center border border-gray-500 focus:outline-none focus:ring-2 rounded-lg ${
+                          incorrectOtp
+                            ? "focus:ring-error-600"
+                            : "focus:ring-blue-500"
+                        }`}
+                        value={otp[idx]} // Bind input value to state
+                        onChangeText={(text) => handleInputChange(text, idx)}
+                       
+                      />
+                    ))}
+                </View>
+                {incorrectOtp && (
+                  <Text className="text-error-600 text-sm font-normal mt-2">
+                    Incorrect OTP, try again.
+                  </Text>
+                )}
+                <View className="flex-row justify-between mt-4">
+                  <Text className="text-gray-600 text-sm font-normal">
+                    Time remaining: 00:59
+                  </Text>
+                  <Text className="text-gray-500 text-sm font-medium">
+                    Resend OTP
+                  </Text>
+                </View>
+              </>
+            )}
+          </>
+        )}
+        {!otpServiceDown && (
+          <View
+            className={`flex-row gap-2 mt-6 ${
+              logedInSuccessfully && "justify-center items-center"
+            }`}
+          >
+            {logedInSuccessfully ? (
+              <Text className="text-gray-800 text-xl font-medium mt-16">
+                You’ve logged in successfully!
+              </Text>
+            ) : (
+              <View className="flex-row gap-2 items-center">
+                <Checkbox
+                    // title="Check me!"
+                    value={isTncChecked}
+                    onValueChange={setIsTncChecked}
+                />
+                <Text className="text-gray-600 text-sm font-normal">
+                  I accept the{" "}
+                  <Text className="text-tangerine-700 text-sm font-medium">
+                    Terms and Conditions
+                  </Text>{" "}
+                  mentioned.
+                </Text>
+              </View>
+            )}
+          </View>
+        )}
+    
+        <View className="mt-4 rounded-2xl bg-blue-gray-50">
+          
+          <Text className="text-gray-600 text-sm font-normal">
+            Your Money Plan Report is linked to your mobile number so that you
+            can track your goal progress anytime.
+          </Text>
+        </View>
+      </View>
+      <View className="mt-8">
+        <TouchableOpacity
+        className={`py-4 rounded-lg ${
+            isVerifyOtpEnabled ? "bg-indigo-600" : "bg-indigo-200"
+          }`}
+          onPress={handleVerifyOtp}
+          disabled={!isVerifyOtpEnabled}
+        >
+            <Text className="text-center text-white text-base font-medium">
+              Verify OTP
+            </Text>
+        </TouchableOpacity>
+      </View>
+      </View>
+    </View>
+  );
+}
